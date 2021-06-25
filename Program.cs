1727b794f0cdeab4156bf9a062829cbb5ad1953b
pedrongomes27 <pedro.ngomes27@hotmail.com>
2021-06-25 12:08:10 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criando_Funções
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int raio;
+            void get_raio()
+            {
+                raio = Convert.ToInt32(Console.ReadLine());
+                double pi = 3.14159;
+                double area = pi * Math.Pow(raio, 2);
+                Console.WriteLine(Math.Round(area, 4));
+
+            }
+
+            get_raio();
+            Console.ReadKey();
+        }
+    }
+}
